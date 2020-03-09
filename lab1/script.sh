3d08cc8d2c4ee268adf5218de4e70488aa6563af
NovikVladimir <44694305+NovikVladimir@users.noreply.github.com>
2020-03-09 22:21:24 +0300

    Add files via upload

---
+++ lab1/script.sh
@@ -0,0 +1,6 @@
+cd docker-hadoop-master
+docker-compose up -d
+id=$(docker ps | grep namenode | AWK '{printf $1}')
+docker cp ../target/lab1-1.0-SNAPSHOT-jar-with-dependencies.jar $id:lab1-1.0-SNAPSHOT-jar-with-dependencies.jar
+docker cp ../../input/000000 $id:000000
+docker cp ../hadoop.sh $id:hadoop.sh
